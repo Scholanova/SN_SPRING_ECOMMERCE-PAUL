2a0bcb1c33bc19c3951b3f04e2fdaf95c3367a03
Pumma <paul.ppauwels@gmail.com>
2020-04-15 11:43:16 +0200

    2/4
    deux premiers teste passer

--- src/main/java/com/scholanova/ecommerce/cart/service/CartServiceImpl.java
+++ src/main/java/com/scholanova/ecommerce/cart/service/CartServiceImpl.java
@@ -1,20 +1,34 @@
 package com.scholanova.ecommerce.cart.service;
 
 import com.scholanova.ecommerce.cart.entity.Cart;
+import com.scholanova.ecommerce.cart.exception.CartException;
+import com.scholanova.ecommerce.product.entity.Product;
+import com.scholanova.ecommerce.product.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class CartServiceImpl implements CartService{
+    @Autowired
+    ProductRepository repository_product;
 
     @Override
-    public Cart addProductToCart(Cart cart, Long productId, int quantity) {
+    public Cart addProductToCart(Cart cart, Long productId, int quantity) throws CartException {
         //TODO
-        return null;
+        try {
+            cart.addProduct(repository_product.findById(productId).get(), quantity);
+            return cart;
+
+        }catch (Exception e){
+            throw new CartException("Unable to add a product to the cart");
+        }
+
     }
 
     @Override
     public Cart changeProductQuantity(Cart cart, Long productId, int quantity) {
         //TODO
+        /*cart.changeProductQuantity(cart.getCartItemByProductName(repository_product.findAllById(productId).get())*/
         return null;
     }
 }
